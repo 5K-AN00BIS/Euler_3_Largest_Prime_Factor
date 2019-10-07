bf78ef8c858b8af11df24f8627b6daad2ba4657c
RYAN BACHMAN <RYABACH1@uat.edu>
2019-10-07 13:14:11 -0700

    Initial commit.

---
+++ src/LargestPrimeFactor.java
@@ -0,0 +1,23 @@
+/*
+    Ryan Bachman
+
+    The prime factors of 13195 are 5, 7, 13 and 29.
+
+    What is the largest prime factor of the number 600851475143?
+ */
+
+public class LargestPrimeFactor
+{
+    public static long getPrimeFactor(long numberToCheck)
+    {
+        long primeFactor = 0L;
+
+        return primeFactor;
+    }
+
+    public static void main(String[] args)
+    {
+        long numberToCheck = 600851475143L;
+        System.out.println("The largest prime factor of " + numberToCheck + " is " + getPrimeFactor(numberToCheck));
+    }
+}
